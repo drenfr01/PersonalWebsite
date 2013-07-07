0d35c420cb4e98b5e95766855a02dd56add9242f
drenfr01 <duncanrenfrow@gmail.com>
2013-07-07 14:06:55 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This will be the application that displays my personal website. It will act as a central location for all my side-projects,
+current interests, etc.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalWebite::Application.config.secret_token = '6bb4b2688fe3e5db24c811c968e8c6370d247cf4c85cc9daa22f00723990e4ac57376d4cdcff26a5f136bed4a3dce799d04cc7deeabaa1ad2d5c4e169d4f92c0'
